382fc6474e83e8e7af060f1465a345c22d03846c
tylerku <ty.udy22@gmail.com>
2017-10-18 18:40:40 -0600

    Basic Facial Recognizer Beginning Stuff

---
+++ Moi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moi
+//
+//  Created by Ty Udy on 10/11/17.
+//  Copyright © 2017 Ty Udy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
